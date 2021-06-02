bfc07bccde0e857385fd634c8179b30dc2a44ae8
Borshchick <19archenemy92@gmail.com>
2021-06-02 17:17:53 +0300

    add function who set color and add nev div "priority" with className and style

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React from "react"
 import {AffairType} from "./HW2"
 import style from "./Affairs.module.css"
 
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: (_id:number)=>void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = (_id: number) => {
-            props.deleteAffairCallback(_id)
+        props.deleteAffairCallback(_id)
     }
+
+    const chooseColor = () => {
+        if (props.affair.priority === "middle") {
+            return {color: "yellow"}
+        }
+        if (props.affair.priority === "high") {
+            return {color: "red"}
+        }
+        if (props.affair.priority === "low") {
+            return {color: "gray"}
+        }
+    }
+
     return (
         <div className={style.content}>
             <div className={style.content_name}>
                 {props.affair.name.toUpperCase()}
+
+            </div>
+            <div className={`${style.content_priority} `} style={chooseColor()}>
+                [{props.affair.priority}]
             </div>
             <div className={style.content_action}>
-                <button onClick={()=>deleteCallback(props.affair._id)}>X</button>
+                <button onClick={() => deleteCallback(props.affair._id)}>X</button>
             </div>
         </div>
     )
