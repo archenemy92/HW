985caf2c0663e45cdd4ab117449a2b355aa31154
Borshchick <19archenemy92@gmail.com>
2021-06-08 12:43:46 +0300

    add new component

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import s from "./App.module.css"
 import HW1 from "../../../p2-homeworks/h1/HW1"
 import HW2 from "../../../p2-homeworks/h2/HW2"
+import HW3 from "../../../p2-homeworks/h3/HW3"
 
 
 
@@ -11,7 +12,7 @@ const App = () => {
             <div>react homeworks:</div>
             <HW1/>
             <HW2/>
-            {/*<HW3/>*/}
+            <HW3/>
             {/*<HW4/>*/}
             {/*<HW5/>*/}
 
