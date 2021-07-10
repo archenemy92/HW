b7669a3910008d81509c5019d352cb66243f6a26
Borshchick <19archenemy92@gmail.com>
2021-07-10 19:19:16 +0300

    create new component

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export function Junior() {
+    return (
+        <div>
+            JUNIOR
+        </div>
+    )
+}
+
+
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
