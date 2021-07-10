958a3c16e1e57a0e0e821d2c80fd5379cf6367d9
Borshchick <19archenemy92@gmail.com>
2021-07-10 22:45:03 +0300

    refactor

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -20,12 +20,9 @@ function Routes() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
                 <Route path={"/"} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
-
                 <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
                 <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
                 <Route path={PATH.JUNIOR_NEXT_LEVEL} render={() => <JuniorNextLevel/>}/>
-
-                // add routes
 
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
                 <Route render={() => <Error404/>}/>
