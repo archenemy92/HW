c0123b6e71dc4235d426e323623cc3d3befa97f4
Borshchick <19archenemy92@gmail.com>
2021-07-11 16:11:01 +0300

    add new props, add useEffect, add setValue in restore function

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react'
-import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
-import {restoreState, saveState} from './localStorage/localStorage'
+import React, {useEffect, useState} from "react"
+import SuperEditableSpan from "./common/c4-SuperEditableSpan/SuperEditableSpan"
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton"
+import {restoreState, saveState} from "./localStorage/localStorage"
+
 
 function HW6() {
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>("")
+    const [error, setError] = useState("")
+    const [editMode, setEditMode] = useState<boolean>(false)
+
+useEffect(()=>{
+    if (value.length === 0) {
+        setError("error: field can't be empty")
+    }
+    if (value) {
+        setError("")
+    }
+
+}, [value])
 
     const save = () => {
-        saveState<string>('editable-span-value', value)
+        saveState<string>("editable-span-value", value)
     }
     const restore = () => {
-        // setValue()
+        setValue(() => restoreState<string>("editable-span-value", value))
     }
 
     return (
@@ -21,12 +34,17 @@ function HW6() {
             {/*should work (должно работать)*/}
             <div>
                 <SuperEditableSpan
+                    label={"Enter Text"}
+                    setErrorHandler={(err:string)=>setError(err)}
+                    isEditMode={()=>setEditMode(!editMode)}
+                    editMode={editMode}
+                    error={error}
                     value={value}
                     onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
+                    spanProps={{children: value ? undefined : "enter text..."}}
                 />
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
+            <SuperButton onClick={save} disabled={!value}>save</SuperButton>
             <SuperButton onClick={restore}>restore</SuperButton>
 
             <hr/>
